45c27142e3b69999a5c361c0787d163075ea75fc
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-02-12 09:15:14 +0200

    .

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ todoButton.addEventListener('click', addTodo);
 
 // Functions
 function addTodo(event) {
-	// Prevent form from submitting
+	// Prevent form from submitting	
 	event.preventDefault();
-	console.log('xds');
+	// Todo div
+	const todoDiv = document.createElement('div');
+	todoDiv.classList.add('todo');
+	// li
+	const newTodo = document.createElement('li');
+	newTodo.innerText = 'ruta';
+	newTodo.classList.add('todo-item');
+	todoDiv.appendChild(newTodo);
+	// Check as completed button
+	const completedButton = document.createElement('button');
+	completedButton.innerHTML = '<i class="fas fas-check"></i>';
+	completedButton.classList.add("complete-btn");
+	todoDiv.appendChild(completeButton);
+	// Check delete button
+	const deletedButton = document.createElement('button');
+	deletedButton.innerHTML = '<i class="fas fas-check"></i>';
+	deletedButton.classList.add("complete-btn");
+	todoDiv.appendChild(deletedButton);
 }
