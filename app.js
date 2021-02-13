73e05cee02d984709ea9094106c5bcddf99662bb
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-02-13 13:42:26 +0200

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheckTodo);
 
 // Functions
 function addTodo(event) {
@@ -22,7 +22,7 @@ function addTodo(event) {
 	// Check as completed button
 	const completeButton = document.createElement('button');
 	completeButton.innerHTML = '<i class="fas fa-check"></i>';
-	completeButton.classList.add("complete-btn");
+	completeButton.classList.add('complete-btn');
 	todoDiv.appendChild(completeButton);
 	// Check delete button
 	const deleteButton = document.createElement('button');
@@ -33,4 +33,18 @@ function addTodo(event) {
 	todoList.appendChild(todoDiv);
 	// Clear Todo input value
 	todoInput.value = '';
+}
+
+function deleteCheckTodo(event) {
+	const clickedItem = event.target;
+	const todo = clickedItem.parentElement; 
+	// Delete todo
+	if (clickedItem.classList[0] === 'delete-btn') {
+		todo.remove();
+	}
+
+	// Delete todo
+	if (clickedItem.classList[0] === 'complete-btn') {
+		todo.classList.toggle('completed');
+	}
 }
